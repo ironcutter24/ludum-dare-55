88d4c0f71bd26ece3bbfeee0da35b1dbf0c56347
Damiano <ironcutter24@gmail.com>
2024-04-10 19:54:25 +0200

    Initial commit

---
+++ scripts/global.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+@export var menu_scene_path: String = ""
+@export var game_scene_path: String = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	if event.is_action_pressed("quit_application"):
+		get_tree().quit()
+
+
+func load_menu_scene():
+	get_tree().change_scene_to_file(menu_scene_path)
+
+func load_game_scene():
+	get_tree().change_scene_to_file(game_scene_path)
