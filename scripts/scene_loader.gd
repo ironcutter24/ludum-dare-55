4e8077577799560abf426e77e4ceef7721bc241f
wadlo <jalen.morgan000@gmail.com>
2024-04-14 08:48:49 -0400

    Fix scene loader bug

--- scripts/scene_loader.gd
+++ scripts/scene_loader.gd
@@ -3,8 +3,10 @@ extends Node
 var sceneLoaded: Node = null;
 
 func load_new_scene(newScene: PackedScene):
+	get_tree().change_scene_to_packed(newScene)
+	"""
 	if (sceneLoaded != null):
 		sceneLoaded.free();
 	var instantiated = newScene.instantiate();
 	sceneLoaded = instantiated;
-	get_tree().root.add_child(instantiated);
+	get_tree().root.add_child(instantiated);"""
