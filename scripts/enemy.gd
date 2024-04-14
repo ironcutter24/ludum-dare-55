affe34e58e4eab5596d89ccc2654216a141e96b4
wadlo <jalen.morgan000@gmail.com>
2024-04-14 14:55:53 -0400

    Improved enemy AI

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,20 +4,26 @@ const STOP_DISTANCE = 20.0
 
 var to_player : Vector2 = Vector2.ZERO
 @export var baseUnit: BaseUnit;
+var last_seen_player_position: Vector2 = Vector2.ZERO;
 
 
 func _physics_process(delta):
 	
-	to_player = (Global.player_controller.global_position - baseUnit.global_position)
+
 	
 	var space_state = get_world_2d().direct_space_state
 	# use global coordinates, not local to node
 	var query = PhysicsRayQueryParameters2D.create(baseUnit.global_position, Global.player_controller.global_position, Global.WALL_MASK)
 	var result = space_state.intersect_ray(query)
 	
-	if result.size() == 0 && to_player.length() > STOP_DISTANCE:
+	if result.size() == 0:
+		last_seen_player_position = Global.player_controller.global_position;
+	
+	to_player = (last_seen_player_position - baseUnit.global_position)
+	
+	if to_player.length() > STOP_DISTANCE and last_seen_player_position != Vector2.ZERO:
 		baseUnit.setMoveDirection(to_player.normalized())
-		baseUnit.setTargetPosition(Global.player_controller.global_position);
+		baseUnit.setTargetPosition(last_seen_player_position);
 		baseUnit.TryAttack();
 	else:
 		var moveDirection = Vector2(0,0);
